a39e1bf53e0affade93b8bcd5d830717f8d777d5
jhsansom <jhsansom@gmail.com>
2020-05-06 13:47:06 -0500

    Modified in preparation for new translation correction method

--- fmtrack/translation_corrector.py
+++ fmtrack/translation_corrector.py
@@ -2,72 +2,93 @@ import numpy as np
 from pyearth import Earth
 import matplotlib.pyplot as plt
 import fmtrack
+from . import tracking
 
 class TranslationCorrector:
 
     def __init__(self):
         pass
 
-    def create_model(self, beads_init, beads_final, cell_init, cell_final, buffer_cell, closest_no_conflict):
-        if cell_init is not None:
-            points_init = cell_init.points
-            points_final = cell_final.points
-        
-            x_min = np.min([np.min(points_init[:,0]),np.min(points_final[:,0])]) - buffer_cell 
-            x_max = np.max([np.max(points_init[:,0]),np.max(points_final[:,0])]) + buffer_cell
-            y_min = np.min([np.min(points_init[:,1]),np.min(points_final[:,1])]) - buffer_cell
-            y_max = np.max([np.max(points_init[:,1]),np.max(points_final[:,1])]) + buffer_cell
-            z_min = np.min([np.min(points_init[:,2]),np.min(points_final[:,2])]) - buffer_cell
-            z_max = np.max([np.max(points_init[:,2]),np.max(points_final[:,2])]) + buffer_cell
+    # identifies 'safe' beads to use for translation correction by using
+    # a bounding box (a rectangular prism) around the cell
+    def bounding_box(self):
 
-        x_pos, y_pos, z_pos = beads_init.get_xyz()
-        x_pos_new, y_pos_new, z_pos_new = beads_final.get_xyz()
-        
-        num_pts = len(x_pos)
-        X = []; Y = []; Z = []; U = []; V = []; W = [] 
+        # defines bounding box
+        points_init = self.cell_init.points
+        points_final = self.cell_final.points
+        x_min = np.min([np.min(points_init[:,0]),np.min(points_final[:,0])]) - self.buffer_cell 
+        x_max = np.max([np.max(points_init[:,0]),np.max(points_final[:,0])]) + self.buffer_cell
+        y_min = np.min([np.min(points_init[:,1]),np.min(points_final[:,1])]) - self.buffer_cell
+        y_max = np.max([np.max(points_init[:,1]),np.max(points_final[:,1])]) + self.buffer_cell
+        z_min = np.min([np.min(points_init[:,2]),np.min(points_final[:,2])]) - self.buffer_cell
+        z_max = np.max([np.max(points_init[:,2]),np.max(points_final[:,2])]) + self.buffer_cell
+
+        # --> limit to points that aren't too close to the cell 
+        X_safe = []; Y_safe = []; Z_safe = []; U_safe = []; V_safe = []; W_safe = [] 
+        num_pts = len(self.U)
         for kk in range(0,num_pts):
-            idx = closest_no_conflict[kk]
-            if idx < len(closest_no_conflict):
-                U.append(x_pos_new[idx] - x_pos[kk])
-                V.append(y_pos_new[idx] - y_pos[kk])
-                W.append(z_pos_new[idx] - z_pos[kk])
-                X.append(x_pos_new[idx]); Y.append(y_pos_new[idx]); Z.append(z_pos_new[idx])
-        
-        if cell_init is not None:
-            # --> limit to points that aren't too close to the cell 
-            X_safe = []; Y_safe = []; Z_safe = []; U_safe = []; V_safe = []; W_safe = [] 
-            num_pts = len(U)
-            for kk in range(0,num_pts):
-                x_out = X[kk] < x_min or X[kk] > x_max
-                y_out = Y[kk] < y_min or Y[kk] > y_max
-                z_out = Z[kk] < z_min or Z[kk] > z_max
-                if x_out or y_out or z_out:
-                    X_safe.append(X[kk])
-                    Y_safe.append(Y[kk])
-                    Z_safe.append(Z[kk])
-                    U_safe.append(U[kk])
-                    V_safe.append(V[kk])
-                    W_safe.append(W[kk])
+            x_out = self.X[kk] < x_min or self.X[kk] > x_max
+            y_out = self.Y[kk] < y_min or self.Y[kk] > y_max
+            z_out = self.Z[kk] < z_min or self.Z[kk] > z_max
+            if x_out or y_out or z_out:
+                X_safe.append(self.X[kk])
+                Y_safe.append(self.Y[kk])
+                Z_safe.append(self.Z[kk])
+                U_safe.append(self.U[kk])
+                V_safe.append(self.V[kk])
+                W_safe.append(self.W[kk])
 
-            X_safe = np.asarray(X_safe); Y_safe = np.asarray(Y_safe); Z_safe = np.asarray(Z_safe)
-            U_safe = np.asarray(U_safe); V_safe = np.asarray(V_safe); W_safe = np.asarray(W_safe)
+        self.X_safe = np.asarray(X_safe); self.Y_safe = np.asarray(Y_safe); self.Z_safe = np.asarray(Z_safe)
+        self.U_safe = np.asarray(U_safe); self.V_safe = np.asarray(V_safe); self.W_safe = np.asarray(W_safe)
+
+
+    # identifies 'safe' beads to use for translation correction by using
+    # only beads that are above a certain threshold from the cell boundary
+    def distance_threshold(self):
+        pass
+
+    # used only when there is no cell, or you want to include all of the beads in translation correction
+    def no_cell(self):
+        self.X_safe = self.X
+        self.Y_safe = self.Y
+        self.Z_safe = self.Z
+        self.U_safe = self.U
+        self.V_safe = self.V
+        self.W_safe = self.W
+
+    def create_model(self, beads_init, beads_final, cell_init, cell_final, buffer_cell, closest_no_conflict, use_box=True):
+
+        # initialize objects
+        self.beads_init = beads_init
+        self.beads_final = beads_final
+        self.cell_init = cell_init
+        self.cell_final = cell_final
+        self.buffer_cell = buffer_cell
+        self.beads_init_new, self.beads_final_new = tracking.get_corrected_beads(beads_init, beads_final, closest_no_conflict)
+
+        # get matched positions and displacements
+        self.X, self.Y, self.Z = self.beads_init_new.get_xyz()
+        U, V, W = self.beads_final_new.get_xyz()
+        self.U = U - self.X
+        self.V = V - self.Y
+        self.W = W - self.Z
+
+        # calculate safe beads
+        if cell_init is None:
+            self.no_cell()
         else:
-            X_safe = X; Y_safe = Y; Z_safe = Z
-            U_safe = U; V_safe = V; W_safe = W
-
-        # saved for plotting
-        self.U = U
-        self.V = V
-        self.W = W
-        self.Z = Z
+            if use_box:
+                self.bounding_box()
+            else:
+                self.distance_threshold()
         
         # --> fit MARS models 
         self.model_U = Earth(max_degree=2,max_terms=10)
-        self.model_U.fit(Z_safe,U_safe)
+        self.model_U.fit(self.Z_safe,self.U_safe)
         self.model_V = Earth(max_degree=2,max_terms=10)
-        self.model_V.fit(Z_safe,V_safe)
+        self.model_V.fit(self.Z_safe,self.V_safe)
         self.model_W = Earth(max_degree=2,max_terms=10)
-        self.model_W.fit(Z_safe,W_safe)
+        self.model_W.fit(self.Z_safe,self.W_safe)
 
     def correct_beads(self, beads):
         x_pos, y_pos, z_pos = beads.get_xyz()
